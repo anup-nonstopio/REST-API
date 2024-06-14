f2bd6ae7bd419e8c1f3e0aa1db9cde30cf61563b
Anup Gupta <anup.gupta@nonstopio.com>
2024-06-14 12:54:23 +0530

    learnt: dynamic params

--- index.js
+++ index.js
@@ -19,4 +19,12 @@ app.get('/users', (req, res) => {         //for server side rendering
     return res.send(html);
 })
 
+app.get('/api/users/:id', (req, res) => {
+    const id = Number(req.params.id); // id in params is a string
+
+    const user = users.find((user) => user.id === id); 
+
+    return res.json(user);
+})
+
 app.listen(PORT, () => console.log(`Server started at ${PORT}`))
