02d78693a3963cfbe575adb214c82af94dcb571e
Anup Gupta <anup.gupta@nonstopio.com>
2024-06-14 13:11:23 +0530

    learnt: combine all same routes to a single app.route()

--- index.js
+++ index.js
@@ -19,6 +19,13 @@ app.get('/users', (req, res) => {         //for server side rendering
     return res.send(html);
 })
 
+app.post('/api/users', (req, res) => {
+    //TODO: Create a new user
+    return res.json({ status: 'pending' }); 
+})
+
+//all three req has same route
+/*
 app.get('/api/users/:id', (req, res) => {
     const id = Number(req.params.id); // id in params is a string
 
@@ -26,5 +33,30 @@ app.get('/api/users/:id', (req, res) => {
 
     return res.json(user);
 })
+
+app.patch('/api/users/:id', (req, res) => {
+    //TODO: Edit the user with ID = id
+    return res.json({ status: 'pending' });
+})
+
+app.delete('/api/users/:id', (req, res) => {
+    //TODO: Delete the user with ID = id
+    return res.json({ status: 'pending' });
+})
+*/
+app.route('/api/users/:id')
+    .get((req, res) => {
+        const id = Number(req.params.id); // id in params is a string
+        const user = users.find((user) => user.id === id); 
+        return res.json(user);
+    })
+    .patch((req, res) => {
+        //TODO: Edit the user with ID = id
+        return res.json({ status: 'pending' });
+    })
+    .delete((req, res) => {
+        //TODO: Delete the user with ID = id
+        return res.json({ status: 'pending' });
+    })
 
 app.listen(PORT, () => console.log(`Server started at ${PORT}`))
